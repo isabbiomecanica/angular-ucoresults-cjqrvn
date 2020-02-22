d6cf878b9a388efdc43b653cccde0c64e6c81697
isabbiomecanica <isabbiomecanica@gmail.com>
2020-02-22 14:59:34 +0100

    1 file modified

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,13 @@
 import {NgModule} from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 import {
   MatSidenavModule,
   MatToolbarModule,
@@ -8,7 +16,17 @@ import {
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatListModule ],
-  exports: [ ]
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ]
 })
 export class MaterialModule {}
