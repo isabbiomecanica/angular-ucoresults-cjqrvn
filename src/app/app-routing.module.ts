c270cf4f56d8b3e3182546b8228ed802b7a960b5
isabbiomecanica <isabbiomecanica@gmail.com>
2020-02-23 10:01:39 +0100

    1 file modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PacientesComponent } from './pacientes/pacientes.component';
-// import { HeroesComponent } from './heroes/heroes.component';
+
 
 import { PacienteDetailComponent }  from './paciente-detail/paciente-detail.component';
-// import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 import { DashboardPacienteComponent }   from './dashboard-paciente/dashboard-paciente.component';
-// import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard-paciente', pathMatch: 'full' },
